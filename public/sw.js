0bece3e9f34d1edcb365104c8278a6bd540cc003
kerberosjang3-wq <kerberosjang3@gmail.com>
2026-05-09 21:48:55 +0900

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'stock-alert-v29';
+const CACHE = 'stock-alert-v30';
 const STATIC = ['/', '/css/style.css', '/js/app.js', '/manifest.json', '/icons/logo.png'];
 
 self.addEventListener('install', e => {
